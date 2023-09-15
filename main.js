a4c2f31dc48922f03d5d738769de730059c32be2
ashish9170 <128040927+ashish9170@users.noreply.github.com>
2023-09-16 04:42:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Android Developer", "Coder"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
